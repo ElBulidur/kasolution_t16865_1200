69fd828beb0140b926dc10da28549226a4c126e2
Julio Pereira <juliopereira.dev@gmail.com>
2024-09-04 18:40:38 -0300

    inicio aula 03

---
+++ aula_03/exercicio.py
@@ -0,0 +1,27 @@
+"""
+    Laboratório 1
+
+    Neste laboratório será refeito o exercício do Caixa Eletrônico, apresentado no laboratório do Módulo 1. A
+    diferença é que aqui deverá ser verificado se o valor do saque é múltiplo de 5. Se não for, apresentar uma
+    mensagem informado da impossibilidade de realização do saque.
+
+"""
+
+
+
+"""
+    Laboratório 2
+
+    Escrever um programa que solicite ao usuário:
+
+        - O nome de um funcionário;
+        - Seu salário.
+
+    Se o salário for superior a R$ 5.000,00 ele terá um desconto de 10% sobre o que ultrapassar R$ 5.000,00.
+    No final, apresentar:
+
+        - O nome do funcionário;
+        - O salário bruto;
+        - O valor do desconto;
+        - O salário líquido.
+"""
